2687013dd31964f2832614bba1628b090b6e0615
Yusa <24yusa@gmail.com>
2018-10-30 12:18:18 +0900

    First commit

---
+++ audio.js
@@ -0,0 +1,171 @@
+function OnButtonClick() {
+
+  // canvas要素を取得
+  var c = document.getElementById('canvas');
+  var cw;
+  var ch;
+
+  // canvasサイズをwindowサイズにする
+  c.width = cw= window.innerWidth*0.8;
+  //cw = window.innerWidth / 1.0;//バーの横の大きさに影響する
+  c.height = ch = window.innerHeight * 0.8;//波の領域の高さ
+
+  //  描画に必要なコンテキスト(canvasに描画するためのAPIにアクセスできるオブジェクト)を取得
+  var ctx = c.getContext('2d');
+
+  // AudioNodeを管理するAudioContextの生成
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  /**
+   * 音声ファイルローダー
+   */
+  var Loader = function (url) {
+    this.url = url;  // �ǂݍ��މ����f�[�^��URL
+  };
+
+  // XMLHttpRequestを利用して音声データ(バッファ)を読み込む。
+  Loader.prototype.loadBuffer = function () {
+    var loader, request;
+    loader = this;
+    request = new XMLHttpRequest();
+    request.open('GET', this.url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function () {
+      // 取得したデータをデコードする。
+      audioCtx.decodeAudioData(this.response, function (buffer) {
+        if (!buffer) {
+          console.log('error');
+          return;
+        }
+        loader.playSound(buffer);  // デコードされたデータを再生する。
+      }, function (error) {
+        console.log('decodeAudioData error');
+      });
+    };
+
+    request.onerror = function () {
+      console.log('Loader: XHR error');
+    };
+
+    request.send();
+  };
+
+  // 読み込んだ音声データ(バッファ)を再生と波形データの描画を開始する。
+  Loader.prototype.playSound = function (buffer) {
+
+    // var gainNode = context.createGain();
+    // buffer.connect(gainNode);
+    // gainNode.connect(context.destination);
+    // gainNode.gain.volue = 0.5;
+    var visualizer = new Visualizer(buffer);
+  };
+
+
+  /**
+   * ビジュアライザー
+   */
+  var Visualizer = function (buffer) {
+    this.sourceNode = audioCtx.createBufferSource();  // AudioBufferSourceNodeを作成
+    this.sourceNode.buffer = buffer;                  // 取得した音声データ(バッファ)を音源に設定
+
+    this.analyserNode = audioCtx.createAnalyser();    // AnalyserNodeを作成
+    this.freqs = new Uint8Array(this.analyserNode.frequencyBinCount);  // 周波数領域の波形データを格納する配列を生成 
+    this.sourceNode.connect(this.analyserNode);       // AudioBufferSourceNodeをAnalyserNodeに接続
+    this.analyserNode.connect(audioCtx.destination);  // AnalyserNodeをAudioDestinationNodeに接続
+
+    // this.gainNode = analyserNode.createGain();
+    // this.sourceNode.connect(this.gainNode);
+    // this.gainNode.gain.value = 0.5;
+    // this.gainNode.connect(audioCtx.destination);
+
+    this.sourceNode.start(0);                         // 再生開始
+    this.draw();                                      // 描画開始
+  };
+
+  var R_num = 120.0;
+  var G_num = 200.0;
+  var B_num = 0.0;
+
+  var B_c = 0;
+
+  Visualizer.prototype.draw = function () {
+    // 0~1まで設定でき、0に近いほど描画の更新がスムーズになり, 1に近いほど描画の更新が鈍くなる。
+    this.analyserNode.smoothingTimeConstant = 0.7;
+
+    // FFTサイズを指定する。デフォルトは2048。
+    this.analyserNode.fftSize = 2048;
+
+    // 周波数領域の波形データを引数の配列に格納するメソッド。
+    // analyserNode.fftSize / 2の要素がthis.freqsに格納される。今回の配列の要素数は1024。
+    this.analyserNode.getByteFrequencyData(this.freqs);
+
+    // 全ての波形データを描画するために、一つの波形データのwidthを算出する。
+    var barWidth = cw / this.analyserNode.frequencyBinCount;
+
+    //ここで黒で書き直すことで画面がリセットされる
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+    ctx.fillRect(0, 0, cw * 1, ch);          //左下と右上の点を指定して四角形を描画
+    ctx.lineWidth = 10;
+
+    // analyserNode.frequencyBinCountはanalyserNode.fftSize / 2の数値。よって今回は1024。
+    for (var i = 0; i < this.analyserNode.frequencyBinCount; i += 8) {
+      var value1 = this.freqs[i];        // 配列には波形データ 0 ~ 255までの数値が格納されている。
+      var percent1 = value1 / 255;       // 255が最大値なので波形データの%が算出できる。
+      //if (percent1*100 > 5) percent1 = 5/100;
+      if (percent1*100 < 2) percent1 = 2/100;
+
+      var height1 = ch * percent1 * 0.9; // %に基づく描画する高さを算出
+      var height2 = ch * percent1 * 0.3; // %に基づく描画する高さを算出
+
+
+      //ここから色を変えるところ
+      // if (B_c == 0) {
+      //   B_num -= i/120;
+      // }
+      // else if (B_c == 1) {
+      //   B_num += i/120;
+      // }
+
+      // if (B_num < 0) {
+      //   B_num = 0;
+      //   B_c = 1;
+      // }
+      // else if (B_num > 255) {
+      //   B_num = 255;
+      //   B_c = 0;
+      // }
+      //ここまで
+      //四角形にグラデーションを書く
+      var grd1 = ctx.createLinearGradient((i) * barWidth, ch / 1.5, barWidth, -height1 / 1.5);
+      grd1.addColorStop(0,"white");
+      grd1.addColorStop(1,"yellow");
+      ctx.fillStyle = grd1;
+      ctx.fillRect((i) * barWidth, ch / 1.5, barWidth, -height1 / 1.5);
+      ctx.fillRect((i) * barWidth, ch / 1.5, barWidth, height2 / 1.5)
+
+      //ここからはグラデーションなしのやつ
+      // ctx.fillStyle = 'rgb(' + R_num.toString(10) + ', ' + G_num.toString(10) + ', ' + B_num.toString(10) + ')';
+      // ctx.strokeStyle = 'rgb(255,255,255)';
+      // ctx.fillRect((i) * barWidth, ch / 1.5, barWidth, -height1 / 1.5); // -をつけないと下に描画される
+      // ctx.fillRect((i) * barWidth, ch / 1.5, barWidth, height2 / 1.5);  //下側にも描画
+
+    }
+
+    window.requestAnimationFrame(this.draw.bind(this));
+  };
+
+  // requestAnimationFrameを多くのブラウザで利用するためにprefixの記載
+  var setUpRAF = function () {
+    var requestAnimationFrame = window.requestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+  };
+
+  setUpRAF();
+  var loader = new Loader('sample2.mp3');
+  loader.loadBuffer();
+
+}
